4c6f1979c6cf4b2b6fadbf2608294fb30aa95f54
Frederik Broy <frbroy@airvonfrederik.speedport.ip>
2024-03-11 00:20:18 +0100

    Initial Setup

---
+++ src/main/java/KITCat.java
@@ -0,0 +1,22 @@
+import static spark.Spark.*;
+import java.io.*;
+
+public class KITCat {
+    public static void main(String[] args) {
+        staticFileLocation("/static");
+        get("/hello", (req, res) -> "Hello World");
+        get("/", (req, res) -> {
+            // Read the contents of the HTML file
+            InputStream inputStream = KITCat.class.getClassLoader().getResourceAsStream("KITCat.html");
+            if (inputStream != null) {
+                // Return the contents of the HTML file
+                res.type("text/html"); // Set content type to HTML
+                return org.apache.commons.io.IOUtils.toString(inputStream, java.nio.charset.StandardCharsets.UTF_8);
+            } else {
+                // If the HTML file doesn't exist, return 404 Not Found
+                res.status(404);
+                return "HTML file not found";
+            }
+        });
+    }
+}
